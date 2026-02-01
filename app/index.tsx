4418a8a7c9d9432d796da0eada792acf34676cfd
itzSerag <serag.eldien.mahmoud@gmail.com>
2026-02-01 19:28:42 +0200

    ReactNative with expo - tailnative starter

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-9xl font-bold text-blue-200 text-pretty text-justify">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
